39a8e22c6ad3975e40052f18c7bc177349c02d1f
Djama <59183324+Djama-Mo@users.noreply.github.com>
2020-05-02 11:47:04 +0300

    Add files via upload

--- PLY/new_pars.py
+++ PLY/new_pars.py
@@ -7,11 +7,9 @@ def main():
     print("Main's starting")
     parser = Parser()
     file_from = open('templates.txt', 'r')
-    # tmp = file_from.read()
     start = time.time()
     for line in file_from:
         parser.CheckString(line.rstrip())
-    # parser.CheckString(tmp)
     end = time.time()
     with open('PLY_time.txt', 'a') as time_file:
         time_file.write(str('{:f}'.format(end - start)) + '\n')
@@ -35,22 +33,17 @@ class Parser:
         self.flag = False
 
     def CheckString(self, string):
-        # self._a.clear()
         self.flag = False
         result = self._parser.parse(string)
         return result
 
-    # def p_address_list(self, p):
-    #     """address_list : address
-    #                     | address_list address"""
-
     def p_address(self, p):
-        """address : NAME SIZE ELEMENTS
-                    | NAME SIZE_ZERO ELEMMH FIG_BR"""
-        if len(p) == 4:
-            p[0] = p[1] + p[2] + p[3]
-        elif len(p) == 5:
-            p[0] = p[1] + p[2] + p[3] + p[4]
+        """address : NAME SIZE SIGNS ELEMENTS END
+                    | NAME SIZE_ZERO EQUAL FIGBR ELEMMH END"""
+        if len(p) == 6:
+            p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
+        elif len(p) == 7:
+            p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
         self.flag = True
         if self._a.get(p[1]) is None:
             self._a[p[1]] = 1
@@ -60,18 +53,6 @@ class Parser:
     def p_err(self, p):
         """err : UNKNOWN"""
         p[0] = p[1]
-
-    # def p_err_list_zero_type(self, p):
-    #     """err_list : """
-    #
-    # def p_err_list_first_type(self, p):
-    #     """err_list : err"""
-    #     p[0] = p[1]
-    #
-    # def p_err_list_second_type(self, p):
-    #     """err_list : err_list err"""
-    #     p[0] = p[1] + p[2]
-
 
     def p_error(self, p):
         print('Unexpected token', p)
@@ -89,20 +70,36 @@ class Parser:
         p[0] = p[1] + p[2] + p[3] + p[4]
 
     def p_address_third_err_type(self, p):
-        """address : NAME SIZE ELEMENTS err NL"""
+        """address : NAME SIZE SIGNS err NL"""
         p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
 
     def p_address_fourth_err_type(self, p):
+        """address : NAME SIZE SIGNS ELEMENTS err NL"""
+        p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+
+    def p_address_fifth_err_type(self, p):
+        """address : NAME SIZE SIGNS ELEMENTS END err NL"""
+        p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7]
+
+    def p_address_sixth_err_type(self, p):
         """address : NAME SIZE_ZERO err NL"""
         p[0] = p[1] + p[2] + p[3] + p[4]
 
-    def p_address_fifth_err_type(self, p):
-        """address : NAME SIZE_ZERO ELEMMH err NL"""
+    def p_address_seventh_err_type(self, p):
+        """address : NAME SIZE_ZERO EQUAL err NL"""
         p[0] = p[1] + p[2] + p[3] + p[4] + p[5]
 
-    def p_address_sixth_err_type(self, p):
-        """address : NAME SIZE_ZERO ELEMMH FIG_BR err NL"""
+    def p_address_eighth_err_type(self, p):
+        """address : NAME SIZE_ZERO EQUAL FIGBR err NL"""
         p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6]
+
+    def p_address_nineth_err_type(self, p):
+        """address : NAME SIZE_ZERO EQUAL FIGBR ELEMMH err NL"""
+        p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7]
+
+    def p_address_tenth_err_type(self, p):
+        """address : NAME SIZE_ZERO EQUAL FIGBR ELEMMH END err NL"""
+        p[0] = p[1] + p[2] + p[3] + p[4] + p[5] + p[6] + p[7] + p[8]
 
     def put_in_file(self):
         for key in self._a.keys():
